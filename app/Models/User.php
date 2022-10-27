77c3d59a2d1250f02bbe0bd0d184a06038324dbb
AEQ Dev <aeqdev2021@gmail.com>
2022-10-27 10:45:46 +0100

    add last login to users

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'last_login_at',
     ];
 
     /**
@@ -40,5 +41,6 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'last_login_at' => 'datetime',
     ];
 }
