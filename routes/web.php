0bc63366703e331d9daefd77e5205cdfa3ae919a
AEQ Dev <aeqdev2021@gmail.com>
2022-10-28 07:39:11 +0100

    fix home link

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    //return ['Laravel' => app()->version()];
-    return csrf_token();
+    //return ['Laravel' => app()->version()];   
 });
 
 require __DIR__ . '/auth.php';
